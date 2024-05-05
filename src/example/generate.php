ab6313bd8ce73b8934d34947e4b6edbf26208dc4
Faiz Nurullah <59213454+Faiznurullah@users.noreply.github.com>
2024-05-05 20:07:06 +0700

    delete 1 parameter

--- src/example/generate.php
+++ src/example/generate.php
@@ -3,6 +3,6 @@ require_once '../gemini.php';
 $apikey = 'API KEY';
 $gemini = new Faiznurullah\Gemini\Gemini($apikey);
 $text = 'Siapa CEO Google Sekarang?';
-$response = $gemini->generateFromText($apikey, $text);
+$response = $gemini->generateFromText($text);
 $response_decode = json_decode($response, true);
 echo $response_decode['candidates'][0]['content']['parts'][0]['text'];
